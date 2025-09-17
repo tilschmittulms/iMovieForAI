69c2ee0d40ce3a9e0bc066194ffc956db565610a
Til Schmitt-Ulms <tilsu262@gmail.com>
2025-09-17 10:40:22 -0700

    Disable no-explicit-any lint rule

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -20,6 +20,11 @@ const eslintConfig = [
       "next-env.d.ts",
     ],
   },
+  {
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off", // 👈 added here
+    },
+  },
 ];
 
 export default eslintConfig;
